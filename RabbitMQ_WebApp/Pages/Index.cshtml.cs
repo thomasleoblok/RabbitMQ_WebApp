019026527aea6fd32de5989821ec86663ada6b0c
Thomas Leo Blok <37294981+thomasleoblok@users.noreply.github.com>
2022-09-07 20:34:20 +0200

    Add project files.

---
+++ RabbitMQ_WebApp/Pages/Index.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RabbitMQ_WebApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+        public void OnPost()
+        {
+            var name = Request.Form["Name"][0];
+            var email = Request.Form["Email"][0];
+            var Tours = Request.Form["Tours"][0];
+            var Book = Request.Form["Book"][0];
+            var Cancel = Request.Form["Cancel"][0];
+
+
+        }
+
+
+
+    }
+}
